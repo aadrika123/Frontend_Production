4cf265c05465cf4c2fd6a2d6e8f59f0228e404b2
aadrika123 <it.info@aadrikaenterprises.com>
2023-05-11 19:06:15 +0530

    changes

--- property_build/assets/GovDcbReport-1ad0ce31.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-28e5a40f.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build/assets/RazorpayPaymentScreen-7ddfc13c.js
+++
@@ -1,1 +0,0 @@
-import{aP as o}from"./index-28e5a40f.js";let t=o("token");console.log("token at basic details is post method...",t);
