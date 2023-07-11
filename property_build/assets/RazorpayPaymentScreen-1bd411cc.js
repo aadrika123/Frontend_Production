05e411aaf362b6124781b8332c7c005cb0eb43df
aadrika123 <it.info@aadrikaenterprises.com>
2023-07-11 13:22:18 +0530

    changes

--- property_build/assets/RazorpayPaymentScreen-1bd411cc.js
+++
@@ -1,1 +0,0 @@
-import{ci as o}from"./index-35c82812.js";let t=o("token");console.log("token at basic details is post method...",t);
---
+++ property_build/assets/RazorpayPaymentScreen-8d8fd964.js
@@ -0,0 +1,1 @@
+import{ci as o}from"./index-2ac17bf1.js";let t=o("token");console.log("token at basic details is post method...",t);
