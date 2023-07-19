3ff0fac07867356761cc49b24fe6d123d67f757a
aadrika123 <it.info@aadrikaenterprises.com>
2023-07-19 12:21:59 +0530

    '19/07/2023'

--- advertisement_build/assets/useSetTitle-bf887386.js
+++
@@ -1,1 +0,0 @@
-import{w as s,o,r as a}from"./index-38bc2bb6.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
---
+++ property_build/assets/RazorpayPaymentScreen-6a111ead.js
@@ -0,0 +1,1 @@
+import{cj as o}from"./index-f1c58318.js";let t=o("token");console.log("token at basic details is post method...",t);
