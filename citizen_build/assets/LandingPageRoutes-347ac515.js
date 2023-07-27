1abf756bc1ef007fb91e4d67e2730bc10b444401
aadrika123 <it.info@aadrikaenterprises.com>
2023-07-27 19:55:55 +0530

    updates

---
+++ citizen_build/assets/LandingPageRoutes-347ac515.js
@@ -0,0 +1,1 @@
+import{j as t,ah as a,ai as e,aj as s}from"./index-c72fe9db.js";const n=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{n as default};
