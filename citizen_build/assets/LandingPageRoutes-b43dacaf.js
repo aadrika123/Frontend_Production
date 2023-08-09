63e534be3b11fc937ab1709721919eca2da72d78
aadrika123 <it.info@aadrikaenterprises.com>
2023-08-09 17:22:27 +0530

    '\09-08-2023'

--- citizen_build/assets/LandingPageRoutes-b43dacaf.js
+++
@@ -1,1 +0,0 @@
-import{j as t,ah as a,ai as e,aj as s}from"./index-a234ce2c.js";const n=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{n as default};
