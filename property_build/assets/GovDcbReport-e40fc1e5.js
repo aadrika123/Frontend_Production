887c51699b6acce0de9df7e8345eb72ffab9641e
aadrika123 <it.info@aadrikaenterprises.com>
2023-08-17 11:23:52 +0530

    option loader

--- property_build/assets/GovDcbReport-e40fc1e5.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ba2f169d.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
---
+++ property_build/assets/QrCode-1347a6f5.js
@@ -0,0 +1,1 @@
+import{j as r,cK as s}from"./index-52722bd1.js";function t(e){return r.jsx(s,{value:e.url,size:e.size})}export{t as Q};
