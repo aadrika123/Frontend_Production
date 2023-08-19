62edfad9a8be064cd626e4bc1e9b303a5ddbecc5
aadrika123 <it.info@aadrikaenterprises.com>
2023-08-19 18:56:38 +0530

    'chagen'

---
+++ advertisement_build/assets/SelfAdvtUpdateForm-6e247925.js
@@ -0,0 +1,1 @@
+import{h as a,j as d}from"./index-448e3e94.js";function t(){const{id:e}=a();return console.log("can edit param received id",e),d("div",{children:"SelfAdvtUpdateForm"})}export{t as default};
