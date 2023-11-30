5e3baa66022051c892fdfc5029d1502d073448d3
aadrika123 <it.info@aadrikaenterprises.com>
2023-11-30 15:16:30 +0530

    30-11-2023

--- Command.bat
+++ Command.bat
@@ -1,7 +1,11 @@
-@ECHO off
-ECHO ===============
-ECHO GIT Command 
-ECHO ===============
-FOR /F "tokens=*" %%a IN ('DATE /T') DO SET arg1=%%a
-git add . & git commit -m %arg1% & git push origin master & git status
-PAUSE
+@echo off
+
+echo ================
+echo GIT PUSH COMMAND
+echo ================
+
+for /f "tokens=*" %%a in ('DATE /t') do set currentDate=%%a
+
+git add . & git commit -m "%currentDate%" & git push origin master & git status
+
+pause
