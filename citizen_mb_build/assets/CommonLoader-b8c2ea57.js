8017e6072854efae7695d008658e04865df18755
Talib Hussain <talibplay111@gmail.com>
2024-04-18 18:28:32 +0530

    updates 18/4/2024, 6:28:31 pm

--- citizen_mb_build/assets/CommonLoader-b8c2ea57.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b446bed8.js";import{B as o}from"./BeatLoader-f38f7c0c.js";const a=()=>e("div",{className:"h-screen w-screen absolute overflow-hidden backdrop-blur-sm top-0",style:{zIndex:999},children:e(o,{className:"absolute z-10 top-[50%] right-[50%] backdrop-blur-sm"})});export{a as default};
--- citizen_mb_build/assets/WaterApplicationPayment-3056a025.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-b446bed8.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
