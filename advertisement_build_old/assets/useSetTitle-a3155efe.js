bfa218c6656cf080806cf6da08a7cddf040d1df2
Talib Hussain <talibplay111@gmail.com>
2024-04-23 18:59:44 +0530

    updates 23/4/2024, 6:59:43 pm

--- advertisement_build_old/assets/useSetTitle-a3155efe.js
+++
@@ -1,1 +0,0 @@
-import{v as s,m as o,r as a}from"./index-96df39d6.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
---
+++ citizen_mb_build/assets/WaterApplicationPayment-f5e2d291.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ebb70fb5.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
