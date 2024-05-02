1dd73553148ebf916c2b19c9be240d4743fc099b
Talib Hussain <talibplay111@gmail.com>
2024-05-02 15:56:03 +0530

    changes

---
+++ citizen_mb_build/assets/useSetTitle-eea3fe7b.js
@@ -0,0 +1,1 @@
+import{R as s,g as o,r as a}from"./index-a794ec81.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
--- citizen_mb_build_old/assets/WaterApplicationPayment-15766947.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-15f15da2.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
