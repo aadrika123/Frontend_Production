fcb60a1da3556002ebb519688e17045cecf28d2d
akshay2s <akshay.aadrika@gmail.com>
2024-06-01 18:13:41 +0530

    changes

---
+++ citizen_mb_build/assets/ListLoader-ffbb107a.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fd80cd87.js";import{R as e}from"./Hourglass-e4a77f53.js";function i({}){return t("div",{className:"flex justify-center items-center",children:t(e,{strokeColor:"#000",width:35,strokeWidth:"3"})})}export{i as R};
--- citizen_mb_build_old/assets/WaterApplicationPayment-451efd05.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-dc2e80f3.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
