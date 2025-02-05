a4d070bc4745f7d5a03c15d8392530d7c44ce3bc
Dhanpati <it.info@aadrikaenterprises.com>
2025-02-05 17:50:26 +0530

    Dhanpati Pushed | Pushed Parking_app | Instructed by Kaushal

---
+++ parking_app_build/assets/index-BIn1-F1h.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B53p_Gm4.js";import"./router-DLL89Qev.js";import"./axios-DmypytPv.js";import"./headlessui-DXpdh5LM.js";function s(){return r.jsx("div",{children:"Register"})}export{s as default};
