a4f72ae3e292d7f9613a4597e3903eaab4887f57
Chandankumar728 <chandankumar6299068@gmail.com>
2025-02-06 16:36:07 +0530

    new changes

--- citizen_build/assets/FileFetchDataHoldingForAdvertSection-188e4103.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:citizen_build/assets/FileFetchDataHoldingForAdvertSection-200a4044.js
-import"./mui-8e804a04.js";import"./router-87042356.js";import{M as o}from"./index-4e794517.js";import"./index-b08c1470.js";o.setAppElement("#root");
-========
-import"./mui-8e804a04.js";import"./router-87042356.js";import{M as o}from"./index-4e794517.js";import"./index-188c2b4c.js";o.setAppElement("#root");
->>>>>>>> 883788365d698980dbe5910274f324fc95958373:citizen_build/assets/FileFetchDataHoldingForAdvertSection-188e4103.js
--- citizen_build/assets/PrintButton-5161b516.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:citizen_build/assets/PrintButton-5161b516.js
-import{j as t}from"./mui-8e804a04.js";import"./router-87042356.js";import{r as e}from"./index-b08c1470.js";import{e as i}from"./Styles-a2fa8249.js";const m=()=>t.jsx(t.Fragment,{children:t.jsx("div",{className:"fixed bottom-10 text-center  justify-center items-center  w-screen print:hidden z-50",children:t.jsxs("button",{onClick:()=>window.print(),className:i,children:[t.jsx(e,{className:"inline text-lg"}),"print"]})})});export{m as P};
-========
-import{j as t}from"./mui-8e804a04.js";import"./router-87042356.js";import{r as e}from"./index-188c2b4c.js";import{e as i}from"./Styles-a2fa8249.js";const m=()=>t.jsx(t.Fragment,{children:t.jsx("div",{className:"fixed bottom-10 text-center  justify-center items-center  w-screen print:hidden z-50",children:t.jsxs("button",{onClick:()=>window.print(),className:i,children:[t.jsx(e,{className:"inline text-lg"}),"print"]})})});export{m as P};
->>>>>>>> 883788365d698980dbe5910274f324fc95958373:citizen_build/assets/PrintButton-e8982d26.js
