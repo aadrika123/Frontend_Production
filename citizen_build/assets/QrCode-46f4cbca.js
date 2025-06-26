0f0fcba60639a16f4ea608ad658f74026a8bda29
Chandankumar728 <chandankumar6299068@gmail.com>
2025-06-26 14:49:37 +0530

    new changes captcha implemented holding tax calcultaor

---
+++ citizen_build/assets/QrCode-46f4cbca.js
@@ -0,0 +1,1 @@
+import{j as t}from"./mui-8bd1858b.js";import{a4 as i}from"./index-ee1e28c0.js";function u(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{u as Q};
--- citizen_build_old/assets/FileFetchDataHoldingForAdvertSection-d19dada7.js
+++
@@ -1,1 +0,0 @@
-import"./mui-8bd1858b.js";import"./router-56e993c5.js";import{M as o}from"./index-1b4154c5.js";o.setAppElement("#root");
