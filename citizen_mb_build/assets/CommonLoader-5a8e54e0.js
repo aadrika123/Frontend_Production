29a46bb083dd88cb0f7bdd549475123221e6f0ee
Chandankumar728 <chandankumar6299068@gmail.com>
2025-07-01 15:50:48 +0530

    new changes

---
+++ citizen_mb_build/assets/CommonLoader-5a8e54e0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-1a12929c.js";import{B as o}from"./BeatLoader-6ed4bb39.js";const a=()=>e("div",{className:"h-screen w-screen absolute overflow-hidden backdrop-blur-sm top-0",style:{zIndex:999},children:e(o,{className:"absolute z-10 top-[50%] right-[50%] backdrop-blur-sm"})});export{a as default};
---
+++ citizen_mb_build/assets/WaterApplicationPayment-c71c702f.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1a12929c.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
