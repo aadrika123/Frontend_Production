35e744e9a21127906f2104241e56def15539f2ba
Chandankumar728 <chandankumar6299068@gmail.com>
2025-07-10 12:22:57 +0530

    new changes

---
+++ citizen_build/assets/useSetLogout-d4648b92.js
@@ -0,0 +1,1 @@
+import{A as e,b as n,P as i}from"./index-d2083b5f.js";function l(t){const{api_logout:s}=i();console.log("inside citizen logout hook"),e.post(s,{},n()).then(function(o){console.log("logout response via usesetlogout.",o)}).catch(function(o){console.log("logout success...")}).finally(()=>{t()})}export{l as u};
