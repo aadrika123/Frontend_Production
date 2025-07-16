4983ccd82b68b25380c3e2e177b7e66a81194789
rohan8883 <rohansingh9135@gmail.com>
2025-07-16 10:40:54 +0530

    changes

---
+++ property_build/assets/GBSAFDetailsEntry-C9o2KUNj.js
@@ -0,0 +1,1 @@
+import{l as a,bF as i,j as l,bM as o}from"./index-hebbF7A6.js";import{D as r}from"./DetailsFactory-CXbuvnOC.js";function u(){a("GB SAF Details");const{id:t}=i(),{get_GbSafDetailsById:s}=o(),e={api:{api_getAppicationFullDetail:{method:"post",url:s}},detailInfo:{title:"GBSAF Details"},filters:{topButtons:!1}};return l.jsx(r,{detailRules:e,id:t})}export{u as default};
---
+++ property_build/assets/QrCode-RlCc6FIN.js
@@ -0,0 +1,1 @@
+import{j as r,cQ as s}from"./index-hebbF7A6.js";function t(e){return r.jsx(s,{value:e.url,size:e.size})}export{t as Q};
