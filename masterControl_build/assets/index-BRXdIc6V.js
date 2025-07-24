0c7c748d55c03eeb06e2d958f5c3a6c991172ba9
rohan8883 <rohansingh9135@gmail.com>
2025-07-24 12:54:29 +0530

    changes

---
+++ masterControl_build/assets/index-BRXdIc6V.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Kl6h-0ea.js";import{a as e}from"./index-CCH4-xUJ.js";function s(){return t.jsx("div",{})}function a(){return t.jsx(e,{title:"Menu Role List (master)",subTitle:"",children:t.jsx("div",{className:"py-0",children:t.jsx(s,{})})})}export{a as default};
