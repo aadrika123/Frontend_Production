806079f3e006605bbada122cf48778153d7bfc4d
Chandankumar728 <chandankumar6299068@gmail.com>
2025-08-16 17:48:17 +0530

    new changes

---
+++ legal_build/assets/minus-BB-pjp8P.js
@@ -0,0 +1,11 @@
+import{e}from"./index-BJOQEx8M.js";/**
+ * @license lucide-react v0.428.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=e("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);/**
+ * @license lucide-react v0.428.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Minus",[["path",{d:"M5 12h14",key:"1ays0h"}]]);export{s as C,a as M};
