06ef6e1d8e04e670be319cebe6737b1ea380acb3
Chandankumar728 <chandankumar6299068@gmail.com>
2025-09-01 15:34:30 +0530

    new changes

--- citizen_build/assets/FileFetchDataHoldingForAdvertSection-540ff2c2.js
+++
@@ -1,1 +0,0 @@
-import"./mui-8bd1858b.js";import"./router-56e993c5.js";import{M as o}from"./index-de7e1d08.js";o.setAppElement("#root");
--- citizen_build/assets/QrCode-a50a864f.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./mui-8bd1858b.js";import{ax as i}from"./index-de7e1d08.js";function u(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{u as Q};
