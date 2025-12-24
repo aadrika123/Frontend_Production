4de8f534268a90b1e63511b9924d2f74fea70570
RajuKumar1213 <rajuvis778@gmail.com>
2025-12-24 13:22:55 +0530

    master control and property build updated

---
+++ property_build/assets/GBSAFDetailsEntry-JCi7myRR.js
@@ -0,0 +1,1 @@
+import{l as a,bE as i,j as l,bL as o}from"./index-BenGTVeO.js";import{D as r}from"./DetailsFactory-DTXq3mZC.js";function u(){a("GB SAF Details");const{id:t}=i(),{get_GbSafDetailsById:s}=o(),e={api:{api_getAppicationFullDetail:{method:"post",url:s}},detailInfo:{title:"GBSAF Details"},filters:{topButtons:!1}};return l.jsx(r,{detailRules:e,id:t})}export{u as default};
