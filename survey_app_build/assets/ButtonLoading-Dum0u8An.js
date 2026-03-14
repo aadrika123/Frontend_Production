1a655c70d7474b422d19df6e9e76c8e73a93d86a
Talib111 <talibplay111@gmail.com>
2026-03-14 12:25:44 +0530

    udpates

--- survey_app_build/assets/ButtonLoading-Dum0u8An.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./vendor-DhZ6duBB.js";import{s as i}from"./ui-CsAjs1aB.js";import{B as r}from"./index-DLRrtabT.js";function a({isLoading:a,children:e,...n}){return s.jsxs(r,{...n,disabled:a,children:[a&&s.jsx(i,{className:"mr-2 h-4 w-4 animate-spin"}),a?"Please wait...":e]})}export{a as B};
