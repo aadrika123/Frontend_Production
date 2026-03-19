f48edc2332ee73229c9d8751f925b031797f3fbe
RajuKumar1213 <rajuvis778@gmail.com>
2026-03-19 17:23:59 +0530

    Updated the wt crm build

---
+++ wt_crm_build/assets/IconChecks-CuzvOzmi.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-CFVNIfCp.js";var o=e("outline","checks","IconChecks",[["path",{d:"M7 12l5 5l10 -10",key:"svg-0"}],["path",{d:"M2 12l5 5m5 -5l5 -5",key:"svg-1"}]]);export{o as I};
